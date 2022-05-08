db79bb06a7cb743b5515022165a9e9fefe9a0b8a
prodhanm <mustafizur.prodhan@gmail.com>
2022-05-08 10:45:01 -0400

    Commiting .gitignore and .toml file. Need work on .py.

---
+++ cipher.py
@@ -0,0 +1,18 @@
+class Codex:
+    def __init__(self, text, step):
+        self.text = text
+        self.step = step
+
+    def encrypt_method(self):
+        encrypted_str = ""
+        for char in self.text:
+            if not char.isalpha():
+                encrypted_str += char
+            elif char.isupper():
+                encrypted_str += chr(((ord(char) + self.step - 65) % 26) + 65)
+            else:
+                encrypted_str += chr(((ord(char) + self.step - 97) % 26) + 97)
+        return encrypted_str
+text1 = Codex.encrypt_method("Trustn01!", 2)
+print(text1.text, text1.step)
+
