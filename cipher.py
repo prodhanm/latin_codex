f91a5b5367fcf7936ea9e3772f5cebebd55d7c47
prodhanm <mustafizur.prodhan@gmail.com>
2022-05-09 22:56:28 -0400

    ensured that the decrypt_method() function works.

--- cipher.py
+++ cipher.py
@@ -13,6 +13,20 @@ class Codex:
             else:
                 encrypted_str += chr(((ord(char) + self.step - 97) % 26) + 97)
         return encrypted_str
+
+    def decrypt_method(self):
+        decrypted_str = ""
+        for char in self.encrypt_method():
+            if not char.isalpha():
+                decrypted_str += char
+            elif char.isupper():
+                decrypted_str += chr(((ord(char) - self.step - 65) % 26) + 65)
+            else:
+                decrypted_str += chr(((ord(char) - self.step - 97) % 26) + 97)
+        return decrypted_str
+
+
 text1 = Codex("Trustn01!", 2)
 print(text1.encrypt_method())
+print(text1.decrypt_method())
 
