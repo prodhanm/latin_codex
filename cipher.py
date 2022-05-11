2382030abcb4f6ac36a768fb79f343ac15c1b243
prodhanm <mustafizur.prodhan@gmail.com>
2022-05-11 17:38:55 -0400

    modified to express non-Alphanumeric code.

--- cipher.py
+++ cipher.py
@@ -7,7 +7,7 @@ class Codex:
         encrypted_str = ""
         for char in self.text:
             if not char.isalpha():
-                encrypted_str += char
+                encrypted_str += chr(((ord(char) + self.step - 32) % 15) + 32)
             elif char.isupper():
                 encrypted_str += chr(((ord(char) + self.step - 65) % 26) + 65)
             else:
@@ -18,15 +18,14 @@ class Codex:
         decrypted_str = ""
         for char in self.encrypt_method():
             if not char.isalpha():
-                decrypted_str += char
+                decrypted_str += chr(((ord(char) - self.step - 32) % 15) + 32)
             elif char.isupper():
                 decrypted_str += chr(((ord(char) - self.step - 65) % 26) + 65)
             else:
                 decrypted_str += chr(((ord(char) - self.step - 97) % 26) + 97)
         return decrypted_str
 
-
-text1 = Codex("Trustn01!", 2)
-print(text1.encrypt_method())
-print(text1.decrypt_method())
+    
+text1 = Codex("Star Trek: The Next Generation!", 2)
+print(text1.encrypt_method(), "=", text1.decrypt_method())
 
